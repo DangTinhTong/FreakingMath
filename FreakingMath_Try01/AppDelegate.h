619fcfa2e9ff9b30a153aafa3e49416c7476c92c
admin <dangtinhtong@gmail.com>
2017-10-01 00:30:12 +0700

    Initial Commit

---
+++ FreakingMath_Try01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreakingMath_Try01
+//
+//  Created by admin on 10/1/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FreakingMath_Try01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FreakingMath_Try01
+//
+//  Created by admin on 10/1/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

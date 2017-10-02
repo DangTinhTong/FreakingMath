c953c99fca42d9cd526e0efe0d380fa068198881
admin <dangtinhtong@gmail.com>
2017-10-02 13:01:55 +0700

    - Xử lý kết quả cộng và ket quả trừ gán vào ketQuaLabel (020917)

--- FreakingMath_Try01/ViewController.m
+++ FreakingMath_Try01/ViewController.m
@@ -13,9 +13,11 @@
 {
     int so1; // Khai bao so thu nhat cho label 1
     int so2; // Khai bao so thu 2 cho label 2
+    NSString *phepToan;
     NSArray *arrPhepToan ; // Khai báo mảng chứa các phần tử cộng, trừ,
     NSArray *arrKetQuaCong; // Mảng chưa kết quả cộng
     NSArray *arrKetQuaTru;  // Mảng chứa kết quả trừ
+    NSArray *ketQua;
 }
 @end
 
@@ -57,8 +59,76 @@
     //Set text for label
     [self.soThu2Label setText:[NSString stringWithFormat:@"%d",so2]];
     
-    
+    //*********************************
+    // 02 Oct
     // Khởi tạo mảng chưa các phép toán
+    // Mảng chứa hai phép toán cộng và trừ
+      arrPhepToan=[[NSArray alloc] initWithObjects:@"+",@"-", nil];
+    /*
+     
+     arrPhepToan=[[NSArray alloc] initWithObjects:@"+",@"-", nil];
+    
+       - Phải tạo ra mảng chứa các phần tử cộng, trừ trước
+     
+     */
+    
+    
+    
+      phepToan =[arrPhepToan objectAtIndex:arc4random()%2];
+    /*
+     phepToan =[arrPhepToan objectAtIndex:arc4random()%2];
+     - Phép toán lấy ngẫu nhiên một trong hai phép toán cộng hoặc trứ
+
+     */
+    
+       [self.phepToanLabel setText:phepToan];
+    /*
+      [self.phepToanLabel setText:phepToan];
+     ==> Gọi và gán cho phepToanLable ngẫu nhiên phép toán cộng hoặc trừ
+     
+     */
+    
+    // Xử lý phần kết quả
+    // Khởi tao mảng chứa kết quá
+    // Kết quả cộng
+   arrKetQuaCong=[[NSArray alloc] initWithObjects:
+                   [NSString stringWithFormat:@"%d",(so1+so2)],
+                   [NSString stringWithFormat:@"%d",(so1+so2)+1],
+                   [NSString stringWithFormat:@"%d",(so1+so2)],
+                   [NSString stringWithFormat:@"%d",(so1+so2)-1],
+                   [NSString stringWithFormat:@"%d",(so1+so2)],
+                   [NSString stringWithFormat:@"%d",(so1+so2)+2],
+                   [NSString stringWithFormat:@"%d",(so1+so2)],
+                   [NSString stringWithFormat:@"%d",(so1+so2)-2],
+                   nil];
+    
+    /* Tạo ra mảng kết quả cộng có các thành  phần từ như trên
+     Xác suất để có một kết quả đúng là 1/5 tức là 20%
+     ==> Chúng ta có thể tạo ra khả năng đungs ít hơn bằng cách thêm các phần từ vào mảng
+     + Mỗi một cái sai thì có 1 các đúng. Gọi là đồng khả năng
+    */
+    //Kết quả trừ
+    
+    //  Tương tự như kết quả trừ cũng vậy
+    arrKetQuaTru=[[NSArray alloc] initWithObjects:
+                   [NSString stringWithFormat:@"%d",(so1-so2)],
+                   [NSString stringWithFormat:@"%d",(so1-so2)+1],
+                   [NSString stringWithFormat:@"%d",(so1-so2)],
+                   [NSString stringWithFormat:@"%d",(so1-so2)-1],
+                   [NSString stringWithFormat:@"%d",(so1-so2)],
+                   [NSString stringWithFormat:@"%d",(so1-so2)+2],
+                   [NSString stringWithFormat:@"%d",(so1-so2)],
+                   [NSString stringWithFormat:@"%d",(so1-so2)-2],
+                   nil];
+   // Gán kêts quá bằng cách ngầu nhiên giống như với phép toán
+    if([phepToan isEqual:@"+"]){
+        
+        ketQua=[arrKetQuaCong  objectAtIndex:arc4random()%(arrKetQuaCong.count)];
+    [self.ketQuaLabel setText:[NSString stringWithFormat:@"%@", ketQua]];
+    }else{
+        ketQua=[arrKetQuaTru objectAtIndex:arc4random()%(arrKetQuaTru.count)];
+        [self.ketQuaLabel setText:[NSString stringWithFormat:@"%@",ketQua]];
+    }
 }
 
 
